3b4ec3356010c196804fbb9f0df9fc2969e08966
Rakhi EM <rakhiemraj@gmail.com>
2022-11-30 14:32:21 +0530

    completed the code

--- pallindrome.js
+++ pallindrome.js
@@ -1,10 +1,10 @@
 \\check palindrome
 
-
+let str="racecar";
 let reverse="";
     for(let i=((str.length)-1);i>=0;i--)
     {
-        reverse=str[i];
+        reverse+=str[i];
     }
     if (str==reverse){
         console.log("Yes");
--- prime.js
+++ prime.js
@@ -1,13 +1,14 @@
 //Check prime
-let check=true;
-    for (let i=2;i<num;i++){
+let num=15;
+let check=0;
+    for (let i=1;i<=num;i++){
         if (num%i==0){
-            check=false;
+            check++;
         }
         
         
     }
-    if (check==false){
+    if (check==2){
         console.log("Yes");
     }
     else {
